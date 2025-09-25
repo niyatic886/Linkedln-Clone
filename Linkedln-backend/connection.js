72ca6236702b8b2d075327b5976d05523e86bd8d
Niyati Chaurasia <163415221+niyatic886@users.noreply.github.com>
2025-09-25 12:56:40 +0530

    Update connection.js

--- Linkedln-backend/connection.js
+++ Linkedln-backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://niyatic886_db_user:1TF7hz69crZ36uv4@cluster0.bfzlc33.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(res=>{
+mongoose.connect('mongodb').then(res=>{
     console.log("Database connected");
 })
 .catch(err=>{
